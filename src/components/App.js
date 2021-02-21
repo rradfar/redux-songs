ad31f3fb7f71cf2b34d3b976e938bb70f9ed2411
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-02-20 16:54:20 -0800

    Render the song list

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import SongList from './SongList';
 
 const App = () => {
-  return <div>Hi from App component!</div>;
+  return (
+    <div className='ui basic segment'>
+      <div className='ui container grid'>
+        <div className='ui row'>
+          <div className='column eight wide'>
+            <SongList />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
